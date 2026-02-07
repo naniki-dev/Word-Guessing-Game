542f32988528793dfd63c5f131459863cfcca70e
LethaboKgasi <lethabomonyuku@gmail.com>
2026-02-07 19:32:54 +0200

    Added tests.

--- test_hangman.py
+++ test_hangman.py
@@ -1,12 +1,23 @@
 import pytest
+from hangman import generates_blank_word, correct
 
-def test_function_1():
-    ...
+def test_generates_blank_word_length():
+    word = "python"
+    blank = generates_blank_word(word)
+    assert blank == "______"  # 6 underscores
 
 
-def test_function_2():
-    ...
+def test_correct_reveals_letter():
+    word = "python"
+    blank = "______"
+    letter = "o"
+    new_blank = correct(blank, letter, word)
+    assert new_blank == "____o_"
 
 
-def test_function_n():
-    ...
+def test_correct_reveals_multiple_letters():
+    word = "banana"
+    blank = "______"
+    letter = "a"
+    new_blank = correct(blank, letter, word)
+    assert new_blank == "_a_a_a" 
